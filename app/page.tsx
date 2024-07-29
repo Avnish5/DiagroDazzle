78f25ae2e25fff1a6d1b267ca5225993754ccd55
Avnish5 <Avnein4988@gmail.com>
2024-07-29 21:41:36 +0530

    implement the header for our webapp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Hero from "./_components/Hero";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h1>hello</h1>
+      <Header />
+      <Hero />
     </div>
   );
 }
