e51123df1ee585997e58dce36c14732f0e55af4b
Avnish5 <Avnein4988@gmail.com>
2024-07-30 22:33:13 +0530

    protect the routes using middleware.ts

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
+    </div>
+  );
 }
 
 export default Dashboard;
