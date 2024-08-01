a073ba3c1b55180dd521df6a5359d025a178d651
Avnish5 <Avnein4988@gmail.com>
2024-08-01 23:13:23 +0530

    create the convex query and mutation

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,32 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
-import React from "react";
+import { api } from "@/convex/_generated/api";
+import {
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import { useMutation, useQuery } from "convex/react";
+import React, { useEffect } from "react";
 
 function Dashboard() {
+  const { user }: any = useKindeBrowserClient();
+  const getUser = useQuery(api.user.getUser, { email: user?.email });
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    if (user) {
+      if (getUser?.length) {
+        createUser({
+          name: user.given_name,
+          email: user.email,
+          image: user.picture,
+        }).then((resp) => {
+          console.log(resp);
+        });
+      }
+    }
+  }, [user]);
+
   return (
     <div>
       Dashboard
