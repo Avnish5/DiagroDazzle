e4deb3d643350f1fa232d96e56d150199bb7c175
Avnish Kumar <68527921+Avnish5@users.noreply.github.com>
2024-08-07 00:46:40 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -21,5 +21,5 @@ export async function middleware(request: NextRequest) {
 // };
 
 export const config = {
-  matcher: ["/dashboard","/workspace","/teams/create"],
+  matcher: ["/dashboard","/workspace/:path*","/teams/:path*"],
 };
